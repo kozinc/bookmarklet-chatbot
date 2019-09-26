fc8890f06e53ad6c808dd8f9f311611c1ae33938
Aljaž Kozina <aljaz@cde.si>
2019-09-26 11:19:05 +0200

    added parameters template and cssURL

--- chatbot-button-lib.js
+++ chatbot-button-lib.js
@@ -1,4 +1,4 @@
-function chatBotLib(tokenId = "97FE6493-A08A-47ED-BBFD-F6F9BE63422C"){
+function chatBotLib(tokenId = '97FE6493-A08A-47ED-BBFD-F6F9BE63422C', template = '', cssURL = 'chatbot-button-lib.css'){
 
     /* Font-awesome */
     var link = document.createElement('link');
@@ -7,100 +7,100 @@ function chatBotLib(tokenId = "97FE6493-A08A-47ED-BBFD-F6F9BE63422C"){
     document.head.appendChild(link);
 
     var chatBotButton = document.createElement('button');
-    chatBotButton.setAttribute("id", "chatBotButton");
-    chatBotButton.setAttribute("class", "btn");
+    chatBotButton.setAttribute('id', 'chatBotButton');
+    chatBotButton.setAttribute('class', 'btn');
     var faiconchat = document.createElement('i');
-    faiconchat.setAttribute("class", "fas fa-comment-alt fa-3x");
+    faiconchat.setAttribute('class', 'fas fa-comment-alt fa-3x');
     chatBotButton.appendChild(faiconchat);
-    chatBotButton.setAttribute("style", "display: block;");
-    /* chatBotButton.innerHTML = "Chat"; */
+    chatBotButton.setAttribute('style', 'display: block;');
+    /* chatBotButton.innerHTML = 'Chat'; */
     document.body.appendChild(chatBotButton);
 
     var style = document.createElement('link');
     style.rel = 'stylesheet';
-    style.href = 'https://ps.cocos.si/demo/css/chatbot-button-lib.css';
+    style.href = 'https://ps.cocos.si/demo/css/' + cssURL;
     document.head.appendChild(style);
     
     function openChatBot() {
-        chatBotButton.setAttribute("style", "display: none;");
-        var chatPopup = document.getElementById("chatPopup");
+        chatBotButton.setAttribute('style', 'display: none;');
+        var chatPopup = document.getElementById('chatPopup');
         if(!!chatPopup) {
-            document.getElementById("chatPopup").style.display = "block"; 
+            document.getElementById('chatPopup').style.display = 'block'; 
 
         } else {
             chatPopup = document.createElement('div');
-            chatPopup.setAttribute("id", "chatPopup");
-            chatPopup.setAttribute("class", "chatBot");
-            chatPopup.style.display = "block";
+            chatPopup.setAttribute('id', 'chatPopup');
+            chatPopup.setAttribute('class', 'chatBot');
+            chatPopup.style.display = 'block';
             
             var closeChatBotButton = document.createElement('button');
-            closeChatBotButton.setAttribute("id", "closeChatBotButton");
-            closeChatBotButton.setAttribute("class", "btn btn-circle");
+            closeChatBotButton.setAttribute('id', 'closeChatBotButton');
+            closeChatBotButton.setAttribute('class', 'btn btn-circle');
             var faiconclose = document.createElement('i');
-            faiconclose.setAttribute("class", "fas fa-times");
-            /* closeChatBotButton.innerHTML = "Close"; */
+            faiconclose.setAttribute('class', 'fas fa-times');
+            /* closeChatBotButton.innerHTML = 'Close'; */
             closeChatBotButton.appendChild(faiconclose);
             chatPopup.appendChild(closeChatBotButton);
-            closeChatBotButton.addEventListener("click", function() {
+            closeChatBotButton.addEventListener('click', function() {
                 closeChatBot();
             });
             
             var refreshChatBotButton = document.createElement('button');
-            refreshChatBotButton.setAttribute("id", "refreshChatBotButton");
-            refreshChatBotButton.setAttribute("class", "btn btn-circle");
+            refreshChatBotButton.setAttribute('id', 'refreshChatBotButton');
+            refreshChatBotButton.setAttribute('class', 'btn btn-circle');
             var faiconrefresh = document.createElement('i');
-            faiconrefresh.setAttribute("class", "fas fa-redo");
-            /* refreshChatBotButton.innerHTML = "Refresh"; */
+            faiconrefresh.setAttribute('class', 'fas fa-redo');
+            /* refreshChatBotButton.innerHTML = 'Refresh'; */
             refreshChatBotButton.appendChild(faiconrefresh);
             chatPopup.appendChild(refreshChatBotButton);
-            refreshChatBotButton.addEventListener("click", function() {
+            refreshChatBotButton.addEventListener('click', function() {
                 refreshChatBot();
             });
             
 
             var minimizeChatBotButton = document.createElement('button');
-            minimizeChatBotButton.setAttribute("id", "minimizeChatBotButton");
-            minimizeChatBotButton.setAttribute("class", "btn btn-circle");
+            minimizeChatBotButton.setAttribute('id', 'minimizeChatBotButton');
+            minimizeChatBotButton.setAttribute('class', 'btn btn-circle');
                 var faiconminimize = document.createElement('i');
-                faiconminimize.setAttribute("class", "fas fa-window-minimize");
-            /* minimizeChatBotButton.innerHTML = "Minimize"; */
+                faiconminimize.setAttribute('class', 'fas fa-window-minimize');
+            /* minimizeChatBotButton.innerHTML = 'Minimize'; */
                 minimizeChatBotButton.appendChild(faiconminimize);
             chatPopup.appendChild(minimizeChatBotButton);
-            minimizeChatBotButton.addEventListener("click", function() {
+            minimizeChatBotButton.addEventListener('click', function() {
                 minimizeChatBot();
             });
 
             var chatBotIframe = document.createElement('iframe');
-            chatBotIframe.setAttribute("id", "chatBotIframe");
-            chatBotIframe.setAttribute("class", "chatBotIframe");
-            var callMeLink = "https://ps.cocos.si/wp/cm/CallMe.html?tokenId=" + tokenId + "&chat=1&video=0&name=Visitor&callerDisplayName=Visitor&subject=Info&comment=&pageId=home";
-            chatBotIframe.setAttribute("src", callMeLink);
-            chatBotIframe.setAttribute("frameborder", 0);
+            chatBotIframe.setAttribute('id', 'chatBotIframe');
+            chatBotIframe.setAttribute('class', 'chatBotIframe');
+            var callMeLink = 'https://ps.cocos.si/wp/cm/CallMe.html?tokenId=' + tokenId + '&chat=1&video=0&name=Visitor&callerDisplayName=Visitor&subject=Info&comment=&template=' + template + '&pageId=home';
+            chatBotIframe.setAttribute('src', callMeLink);
+            chatBotIframe.setAttribute('frameborder', 0);
             chatPopup.appendChild(chatBotIframe);
             document.body.appendChild(chatPopup);
         }
     };
     function minimizeChatBot() {
-        var chatPopup = document.getElementById("chatPopup");
-        chatPopup.style.display = "none";
-        chatBotButton.setAttribute("style", "display: block;");
+        var chatPopup = document.getElementById('chatPopup');
+        chatPopup.style.display = 'none';
+        chatBotButton.setAttribute('style', 'display: block;');
     };
     function refreshChatBot() {
-        var chatBotIframe = document.getElementById("chatBotIframe");
+        var chatBotIframe = document.getElementById('chatBotIframe');
         chatBotIframe.parentNode.removeChild(chatBotIframe);
-        var callMeLink = "https://ps.cocos.si/wp/cm/CallMe.html?tokenId=" + tokenId + "&chat=1&video=0&name=Visitor&callerDisplayName=Visitor&subject=Info&comment=&pageId=home";
-        chatBotIframe.setAttribute("src", callMeLink);
-        chatBotIframe.setAttribute("frameborder", 0);
+        var callMeLink = 'https://ps.cocos.si/wp/cm/CallMe.html?tokenId=' + tokenId + '&chat=1&video=0&name=Visitor&callerDisplayName=Visitor&subject=Info&comment=&template=' + template + '&pageId=home';
+        chatBotIframe.setAttribute('src', callMeLink);
+        chatBotIframe.setAttribute('frameborder', 0);
         
-        document.getElementById("chatPopup").appendChild(chatBotIframe);
+        document.getElementById('chatPopup').appendChild(chatBotIframe);
     };
     function closeChatBot() {
-        var elem = document.getElementById("chatPopup");
+        var elem = document.getElementById('chatPopup');
         elem.parentNode.removeChild(elem);
-        chatBotButton.setAttribute("style", "display: block;");
+        chatBotButton.setAttribute('style', 'display: block;');
     };
     
-    document.getElementById("chatBotButton").addEventListener("click", function() {
+    document.getElementById('chatBotButton').addEventListener('click', function() {
         openChatBot();
     });
 };
